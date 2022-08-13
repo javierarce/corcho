bcd799f143d79b0e00adaf3c5c69f48ecab0f87a
Javier Arce <4933+javierarce@users.noreply.github.com>
2022-08-13 13:19:26 +0200

    Folders

---
+++ js/app.js
@@ -0,0 +1,68 @@
+let $canvas
+
+const getRandomPosition = (img) => {
+  let imageRect = img.getBoundingClientRect()
+  let imageWidth = imageRect.width
+  let imageHeight = imageRect.height
+
+  let rect = window.document.body.getBoundingClientRect()
+  let bodyWidth = Math.max(rect.width, window.innerWidth)
+  let bodyHeight = Math.max(rect.height, window.innerHeight)
+
+  let x = Math.round(Math.random() * (bodyWidth - imageWidth))
+  let y = Math.round(Math.random() * (bodyHeight - imageHeight))
+
+  return { x, y }
+}
+
+const draw = async (data) => {
+
+  let promises = []
+
+  data.forEach((d) => {
+    return new Promise(async (resolve, reject) => {
+      const img = new Image()
+
+      let promise = new Promise(async(resolve) => {
+        img.onload = () => { 
+          resolve(img)
+        }
+      })
+
+      promises.push(promise)
+
+      img.src = `img/${d.filename}`
+      $canvas.appendChild(img)
+    })
+  })
+
+  Promise.all(promises).then((results) => {
+    let height = 0
+
+    results.forEach((image) => {
+      let rect = image.getBoundingClientRect()
+      height+= rect.height
+    })
+
+    window.document.body.style.height = `${height/2}px`
+
+    results.forEach((image) => {
+      let pos = getRandomPosition(image)
+      image.style.top = `${pos.y}px`
+      image.style.left = `${pos.x}px`
+      setTimeout(() => {
+        image.classList.add('is-loaded')
+      }, Math.random() * 2000)
+    })
+  })
+}
+
+const onLoad = () => {
+  $canvas = document.querySelector('.Canvas')
+  fetch('data.json')
+    .then((response) => response.json())
+    .then(draw)
+}
+
+window.onload = onLoad
+
